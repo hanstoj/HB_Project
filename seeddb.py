f7c5e7b148c47d91e479b5f318c1b4ef427dc9be
vagrant <vagrant@vagrant.vm>
2020-11-12 22:47:43 +0000

    seeddb added rest and guest

--- seeddb.py
+++ seeddb.py
@@ -13,28 +13,41 @@ from faker import faker
 fake = Faker()
 
 
-# os.system('dropdb ratings')
-# os.system('createdb ratings')
+os.system('dropdb cafe')
+os.system('createdb cafe')
 
 model.connect_to_db(server.app)
 model.db.create_all()
 
-for restaurant in range(5)
-fake.name()
-fake.password
+for restaurant in range(5):
+    username = fake.username()
+    password = fake.password()
+    restaurant_name = fake.name(length=10)
 
+    restaurant = model.Restaurant(
+        username=username, password=password, restaurant_name=restaurant_name)
+    model.db.session.add(restaurant_name)
+    model.db.session.commit()
 
-# import os
-# import json
-# from random import choice, randint
-# from datetime import datetime
+    for guests in range(50):
+        name = fake.name()
+        phone_num = fake.unique.phone()
 
-# import crud
-# import model
-# import server
+        guest = model.Guest(name=name, phone_num=phone_num)
 
-# os.system('dropdb ratings')
-# os.system('createdb ratings')
+        # for reservation in range(200):
 
-# model.connect_to_db(server.app)
-# model.db.create_all()
+        # import os
+        # import json
+        # from random import choice, randint
+        # from datetime import datetime
+
+        # import crud
+        # import model
+        # import server
+
+        # os.system('dropdb ratings')
+        # os.system('createdb ratings')
+
+        # model.connect_to_db(server.app)
+        # model.db.create_all()
