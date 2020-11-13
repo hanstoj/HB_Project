c15c8d3377ad35365092fe89603b3172d615b57a
vagrant <vagrant@vagrant.vm>
2020-11-13 01:40:02 +0000

    database testing- rest table Faker

--- seeddb.py
+++ seeddb.py
@@ -2,14 +2,13 @@ import model
 import os
 import json
 from random import random
-from datetime import datetime
+import datetime
 
 import crud
-from model import connect_do_db, db, Restaurant,
 import server
+import model
 
-
-from faker import faker
+from faker import Faker
 fake = Faker()
 
 
@@ -20,34 +19,31 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 for restaurant in range(5):
-    username = fake.username()
+    username = fake.word()
     password = fake.password()
-    restaurant_name = fake.name(length=10)
+    restaurant_name = fake.name()
 
     restaurant = model.Restaurant(
         username=username, password=password, restaurant_name=restaurant_name)
-    model.db.session.add(restaurant_name)
+    model.db.session.add(restaurant)
     model.db.session.commit()
 
-    for guests in range(50):
-        name = fake.name()
-        phone_num = fake.unique.phone()
+    # for guests in range(50):
+    #     name = fake.name()
+    #     phone_num = fake.unique.phone()
 
-        guest = model.Guest(name=name, phone_num=phone_num)
+    #     guest = model.Guest(name=name, phone_num=phone_num)
+    #     model.db.session.add(guest)
+    #     model.db.session.commit()
 
-        # for reservation in range(200):
-
-        # import os
-        # import json
-        # from random import choice, randint
-        # from datetime import datetime
-
-        # import crud
-        # import model
-        # import server
-
-        # os.system('dropdb ratings')
-        # os.system('createdb ratings')
-
-        # model.connect_to_db(server.app)
-        # model.db.create_all()
+    # for reservation in range(10):
+    #     res_size = random.randint(1, 4)
+    #     res_time = datetime.today()
+    #     # faker how to get reasonable times
+    #     arrival_time = res_time
+    #     end_time = arrival_time +
+    #     booth_pref = fake.boolean()
+    #     res_notes = db.Column(db.Text)
+    #     celebrating = db.Column(db.Boolean)
+    #     table_id = db.Column(db.Integer, db.ForeignKey('tables.table_id'))
+    #     phone_num = db.Column(db.String, db.ForeignKey('guests.phone_num'))
